3bcd5ce0d1fa4e9be6cdec98281b7f539fdb5800
Looofeee <Looofeee@mail.ru>
2017-12-10 21:34:08 +0300

    v2

--- js/search_youtube.js
+++ js/search_youtube.js
@@ -2,22 +2,40 @@
 	let search_button = document.getElementById('search_button');
 	search_button.addEventListener("click", function(e) {
 			e.preventDefault();
-			var stringSearch = search_text.value;
+			let stringSearch = search_text.value;
+			search(stringSearch);
+	});
+})();
 
+function init(){
+	gapi.client.load("youtube", "v3", function(){
+
+	});
+	gapi.client.setApiKey("AIzaSyAo6BNS_UCYrLS_sEq0O3-jusWqfmAyhWY");
+}
+function search(stringSearch) {
 			var request = gapi.client.youtube.search.list({
 			type:'video',
 			part: 'snippet',
 			q: stringSearch,
-			maxResults:2,
+			maxResults:1,
 			order: "viewCount",
 			});
 			let address;
 			let stringId = "";
 			request.execute(function(response) {
 				for (let i = 0; i < response.result.items.length; i++) {
-					console.log(response.result)
-					console.log(response.result.items[i].snippet.thumbnails.default.url)
-					console.log("    ")
+					
+					console.log(response)
+					let getWatches = gapi.client.youtube.videos.list({
+					id:response.result.items[i].id.videoId,
+					part: 'snippet,contentDetails,statistics',
+					});		
+					getWatches.execute(function(response) {
+						console.log(response.result);
+						a.textContent = response.result.items[0].snippet.localized.title;
+						watches.textContent = response.result.items[0].statistics.viewCount;
+					});
 
 
 					let page_wrapper = document.createElement('div');
@@ -36,24 +54,18 @@
 					image.appendChild(imageTag);
 					imageTag.src = response.result.items[i].snippet.thumbnails.high.url;
 
-					
 					let title = document.createElement('div');
 					title.className = "title";
-					image.appendChild(title)
+					image.appendChild(title);
 
 					let a = document.createElement('a');
 					title.appendChild(a);
+					a.target="_blank";
 					a.href = "https://www.youtube.com/watch?v=" + response.result.items[i].id.videoId;
-					a.textContent  = response.result.items[i].localized;
-
-
 
 					let author = document.createElement('div');
 					let imageAuthor = document.createElement('img');
 					author.className = "author";
-
-					imageAuthor.src = "img/man-user.png"
-					imageAuthor.className = "imageAuthor";
 					
 					page_wrapper_yu.appendChild(imageAuthor)
 					page_wrapper_yu.appendChild(author);
@@ -65,7 +77,6 @@
 					page_wrapper_yu.appendChild(date);
 					date.textContent  = response.result.items[i].snippet.publishedAt;
 
-
 					let watches = document.createElement('div');
 					watches.className = "watches";
 					page_wrapper_yu.appendChild(watches);
@@ -76,35 +87,8 @@
 					description.textContent  = response.result.items[i].snippet.description;
 
 
-					if (i === response.result.items.length) {
-						stringId += response.result.items[i].id.videoId.toString();
-						continue;
-					}
-					stringId += response.result.items[i].id.videoId.toString() + ",";
-
 				//	console.log(response.result.items[i].id.videoId)
 				//	console.log(response.result.items[i].id.videoId)
 				}
-
-					let getWatches = gapi.client.youtube.videos.list({
-					id:stringId,
-					part: 'snippet,contentDetails,statistics',
-					});		
-
-					getWatches.execute(function(response) {
-						for (let i = 0; i < response.result.items.length; i++) {
-							console.log(response.result.items[i].statistics.viewCount);
-						}
-						console.log(response.result);
-					});
 			})
-
-	});
-})();
-
-function init(){
-	gapi.client.load("youtube", "v3", function(){
-
-	});
-	gapi.client.setApiKey("AIzaSyAo6BNS_UCYrLS_sEq0O3-jusWqfmAyhWY");
 }
